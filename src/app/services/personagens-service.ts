2098f838b5de26520f7dd866154b3e1f18388d74
VINICIUS GONCALVES MARTINS <vinicius.gmartins3@senacsp.edu.br>
2025-08-13 22:20:07 -0300

    (feat): aula 08 - lab 1 e 2

---
+++ src/app/services/personagens-service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+
+interface IPersonagem {
+  id: number;
+  nome: string;
+  imagem: string;
+  votos: number;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PersonagensService {
+  personagens: IPersonagem[] = [
+    {
+      id: 113231,
+      nome: 'Rick Sanchez',
+      imagem: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+      votos: 0,
+    },
+    {
+      id: 12312,
+      nome: 'Rick Sanchez',
+      imagem: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg',
+      votos: 0,
+    },
+    {
+      id: 3342342,
+      nome: 'Rick Sanchez',
+      imagem: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg',
+      votos: 0,
+    },
+    {
+      id: 4234234,
+      nome: 'Rick Sanchez',
+      imagem: 'https://rickandmortyapi.com/api/character/avatar/4.jpeg',
+      votos: 0,
+    },
+    {
+      id: 5234234,
+      nome: 'Rick Sanchez',
+      imagem: 'https://rickandmortyapi.com/api/character/avatar/5.jpeg',
+      votos: 0,
+    },
+  ];
+
+  getPersonagens() {
+    return this.personagens;
+  }
+}
